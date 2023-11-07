35b9e6eaa9f13dcc14ba8f17099cb8cf4eea8623
bekizod <bekizodcancer@gmail.com>
2023-11-07 11:15:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="86e84952-1bf7-46a6-9ca4-2a7be01b2abd"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
